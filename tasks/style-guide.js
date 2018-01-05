f58207b9bd365a5bd271114d7907aeb596914601
Glen Cheney <glen@odopod.com>
2018-01-05 14:14:15 -0800

    Upgrade to gulp 4

--- tasks/style-guide.js
+++ tasks/style-guide.js
@@ -1,1 +1,5 @@
-module.exports = [['style-guide--css', 'style-guide--js', 'style-guide--nunjucks']];
+const requirePeer = require('require-linked-peer');
+
+const gulp = requirePeer('gulp');
+
+module.exports = gulp.parallel('style-guide--css', 'style-guide--js', 'style-guide--nunjucks');
